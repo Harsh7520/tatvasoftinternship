6a16a68bd9732e478168c22e3921e422a16cc3d2
Harsh Bhatt <98325507+Harsh7520@users.noreply.github.com>
2022-06-28 11:09:52 +0530

    Add files via upload

---
+++ BookStore.Repositories/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using BookStore.Models.Data;
+using BookStore.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Repositories
+{
+    public class UserRepository
+    {
+        TatvasoftTestContext _context = new TatvasoftTestContext();
+        
+        public List<User> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+        public User Login(LoginModel model)
+        {
+            return _context.Users.FirstOrDefault(c => c.Email.Equals(model.Email.ToLower()) && c.Password.Equals(model.Password));
+        }
+
+        public User Register(RegisterModel model)
+        {
+            User user = new User()
+            {
+                Email = model.Email,
+                Password = model.Password,
+                Firstname = model.Firstname,
+                Lastname = model.Lastname,
+                Roleid = model.Roleid,
+            };
+
+            if (!(_context.Users.Any(c => c.Email.Equals(model.Email.ToLower()))))
+            {
+
+                var entry = _context.Users.Add(user);
+                _context.SaveChanges();
+                return entry.Entity;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ BookStoreAPI/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using BookStore.Repositories;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models.Models;
+using BookStore.Models.Data;
+
+namespace BookStoreAPI.Controllers
+{
+    
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        UserRepository _repository = new UserRepository();
+
+        [HttpGet]
+        [Route("GetUsers")]
+        public IActionResult GetUsers()
+        {
+            return Ok(_repository.GetUsers());
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(LoginModel model)
+        {
+           User user = _repository.Login(model);
+            if (user == null)
+                return NotFound();
+            return Ok(user);
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public IActionResult Register(RegisterModel model)
+        {
+
+          
+            User user = _repository.Register(model);
+            if (user == null)
+                return BadRequest("User Already Exists");
+            return Ok(user);
+        }
+
+
+    }
+}
